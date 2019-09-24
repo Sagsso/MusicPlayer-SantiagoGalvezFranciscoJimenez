0a2ae3df023361b83e5d6885cc81004b159d6d35
SantiagoGalvez <santiagogalvez17@gmail.com>
2019-09-24 00:13:05 -0500

    Playlist ready

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 const play = '<path class="play" fill="#ffffff" d="M3 2l10 6-10 6z"></path>'
 const pause = '<path class="pause" fill="#ffffff" d="M2 2h5v12h-5zM9 2h5v12h-5z"></path>'
 
-let $btnRepro = document.getElementById('btn-repro')
-// let $stressedOut = new AudioPlayer($btnRepro)
-
-let tracks = []
+let tracks = [
+    { src: './assets/songs/SummerDays.mp3', img: './assets/images/SummerDays.jpg', title: 'Summer Days', artist: 'Martin Garrix' },
+    { src: './assets/songs/Heathens.mp3', img: './assets/images/Heathens.jpg', title: 'Heathens', artist: 'Twenty One Pilots' },
+    { src: './assets/songs/UGotThat.mp3', img: './assets/images/UGotThat.jpg', title: 'U Got That', artist: 'Halogen' },
+    { src: './assets/songs/OldTownRoad.mp3', img: './assets/images/OldTownRoad.jpeg', title: 'Old Town Road', artist: 'Lil Nas X ft. Ray Cyrus' },
+    { src: './assets/songs/GodsPlan.mp3', img: './assets/images/GodsPlan.jpg', title: 'GodsPlan', artist: 'Drake' },
+    { src: './assets/songs/LSD.mp3', img: './assets/images/LSD.jpg', title: 'LSD', artist: 'Genius ft. Sia, Diplo' },
+    { src: './assets/songs/NoGuidance.mp3', img: './assets/images/NoGuidance.jpg', title: 'No Guidance', artist: 'Chris Brown ft. Drake' },
+    { src: './assets/songs/Panini.mp3', img: './assets/images/Panini.jpg', title: 'Panini', artist: 'Lil Nas X' },
+    { src: './assets/songs/RightBack.mp3', img: './assets/images/RightBack.jpg', title: 'Right Back', artist: 'Khalid ft. A Boogie' },
+    { src: './assets/songs/StressedOut.mp3', img: './assets/images/stressedout.jpg', title: 'Stressed Out', artist: 'Twenty One Pilots' },
+    { src: './assets/songs/WalkItTalkIt.mp3', img: './assets/images/WalkItTalkIt.jpg', title: 'Walk It Talk It', artist: 'Migos ft. Drake' },
+]
 
 let trackElements = document.getElementsByClassName('track');
-for (let i = 0; i < trackElements.length; i++) {
-    tracks.push(trackElements[i].src);
+// for (let i = 0; i < trackElements.length; i++) {
+//     tracks.push(trackElements[i].src);
+// }
+
+let $playlist = document.getElementById('playlist')
+
+for (let i = 0; i < tracks.length; i++) {
+    let item = document.createElement('li')
+    item.innerHTML = `<span><span class="title">${tracks[i].title}</span> -
+                    <span class="artist">${tracks[i].artist}</span></span><span class="totalTime">2:54</span>`;
+    $playlist.appendChild(item);
 }
+
+$playlist.children[0].classList.add('playing')
+
 
 let myAudioPlayer = new MultimediaPlayer('#main audio', tracks, {
     play: document.querySelector('#btn-repro'),
@@ -70,7 +91,5 @@ $menu.onclick = (e) => {
     document.querySelector('#main').classList.toggle('show');
     document.querySelector('#playlist').classList.toggle('show');
 }
-
-
 
 
--- src/MultimediaPlayer.js
+++ src/MultimediaPlayer.js
@@ -5,7 +5,7 @@ class MultimediaPlayer extends DOMGui {
 
         this._audio = document.querySelector(audioTagSelector);
         this.tracks = tracks;
-        this.audio.src = this.tracks[0];
+        this.audio.src = this.tracks[0].src;
         this.currentTrack = 0;
 
 
@@ -45,11 +45,11 @@ class MultimediaPlayer extends DOMGui {
                 if (this.audio.paused) {
                     this.audio.play();
                     this._DOMElements.play.removeChild(this._DOMElements.play.firstChild);
-                    $btnRepro.innerHTML = pause
+                    this._DOMElements.play.innerHTML = pause
                 } else {
                     this.audio.pause();
                     this._DOMElements.play.removeChild(this._DOMElements.play.firstChild);
-                    $btnRepro.innerHTML = play
+                    this._DOMElements.play.innerHTML = play
                 }
 
                 this._DOMElements.play.classList.toggle('btn-play');
@@ -65,7 +65,7 @@ class MultimediaPlayer extends DOMGui {
         this.addButtonListener('progressBar',
             (e) => {
                 let position = e.offsetX;
-                let totalW = e.target.clientWidth;
+                let totalW = 240;
                 let progress = position / totalW;
                 this.updateProgressBar(progress * 100);
                 this.audio.currentTime = this.audio.duration * progress;
@@ -73,7 +73,11 @@ class MultimediaPlayer extends DOMGui {
 
         this.addButtonListener('back',
             () => {
-                this.changePlayingSong(this.currentTrack - 1);
+                if ((this.currentTrack - 1) >= 0) {
+                    this.changePlayingSong(this.currentTrack - 1);
+                } else {
+                    this.changePlayingSong(this.tracks.length - 1);
+                }
             });
     }
 
@@ -101,11 +105,11 @@ class MultimediaPlayer extends DOMGui {
         if (this.audio.paused) {
             this.audio.play();
             this._DOMElements.play.removeChild(this._DOMElements.play.firstChild);
-            $btnRepro.innerHTML = pause
+            this._DOMElements.play.innerHTML = pause
         } else {
             this.audio.pause();
             this._DOMElements.play.removeChild(this._DOMElements.play.firstChild);
-            $btnRepro.innerHTML = play
+            this._DOMElements.play.innerHTML = play
         }
 
         this._DOMElements.play.classList.toggle('btn-play');
@@ -118,13 +122,15 @@ class MultimediaPlayer extends DOMGui {
         } else {
             this.currentTrack = 0;
         }
-        this.audio.src = this.tracks[this.currentTrack];
+        this.audio.src = this.tracks[this.currentTrack].src;
+        this._DOMElements.cover.style.backgroundImage = `url('${this.tracks[this.currentTrack].img}')`;
         // this.audio.play();
         this.play();
         let playing = this._DOMElements.playlistMenu.querySelector('.playing');
         playing.classList.remove('playing');
         let element = this._DOMElements.playlistMenu.children[this.currentTrack];
         element.classList.add('playing');
+        element.scrollIntoView(true)
         this.setPlayerInfo();
     }
 
@@ -132,6 +138,7 @@ class MultimediaPlayer extends DOMGui {
         let element = this._DOMElements.playlistMenu.children[this.currentTrack];
         this._DOMElements.title.innerHTML = element.querySelector('.title').innerHTML;
         this._DOMElements.artist.innerHTML = element.querySelector('.artist').innerHTML;
+        this._DOMElements.cover.style.backgroundImage = `url('${this.tracks[this.currentTrack].img}')`;
     }
 
     startTimeUpdateListener() {
@@ -141,6 +148,14 @@ class MultimediaPlayer extends DOMGui {
             let progress = current / total;
             this.updateProgressBar(progress * 100);
             this._DOMElements.currentTime.innerHTML = this.formatCurrentTime(current);
+
+            if (current == total && this.audio.paused) {
+                this._DOMElements.play.removeChild(this._DOMElements.play.firstChild);
+                this._DOMElements.play.innerHTML = play
+                let nextTrack = this.currentTrack + 1;
+                setTimeout(this.changePlayingSong.bind(this), 2000, nextTrack);
+            }
+
         }
     }
 
@@ -154,9 +169,9 @@ class MultimediaPlayer extends DOMGui {
 
         if (rounded < 10) {
             result = `${minutes}:0${rounded}`;
-        } else if (rounded >= 10 && rounded < 59) {
+        } else if (rounded >= 10 && rounded <= 59) {
             result = `${minutes}:${rounded}`;
-        } else if (rounded > 60) {
+        } else if (rounded >= 60) {
             rounded = Math.floor(rounded % 60)
             result = (rounded < 10) ? `${minutes}:0${rounded}` : `${minutes}:${rounded}`
             // result = `${minutes}:${rounded}`;
